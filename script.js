3bd1e1b7d86a1c9512fd6cc00779b660354d6f1c
pinguwu <42652753+pinguwu@users.noreply.github.com>
2019-09-12 22:38:59 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+  // define necessary variables
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+
+  // creating the paddle
+  var Paddle = function () {
+    this.x = 125;
+    this.y = 300;
+    
+    this.draw = function(x, y) {
+      ctx.strokeRect(x, y, 200, 20);
+    }
+  }
+  
+  // creating the ball
+  var Ball = function () {
+    this.x = 280;
+    this.y = 100;
+    
+    this.draw = function (x, y) {
+      ctx.beginPath();
+      ctx.arc(x, y, 20, 0, 2 * Math.PI);
+      ctx.stroke();
+    }
+  }
+});
